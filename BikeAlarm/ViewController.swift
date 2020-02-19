c7d2ad4923d9f16eb20fbc5653bd4add327b7722
jtlemkin <jtlemkin@gmail.com>
2020-02-18 17:22:00 -0800

    Implemented UI

--- BikeAlarm/ViewController.swift
+++ BikeAlarm/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var armed = false
+    var bikeInRange = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ToggleAlarm(_ sender: Any) {
+        armed = !armed
+        updateView()
+    }
+    
+    @IBOutlet weak var alarmInRange: UIButton!
+    @IBOutlet weak var inRangeLabel: UILabel!
+    @IBOutlet weak var bikeImage: UIImageView!
+    
+    func updateView() {
+        alarmInRange.backgroundColor = armed ? UIColor.gray : UIColor.red
+        alarmInRange.setTitle(armed ? "Disarm" : "Arm", for: .normal)
+        inRangeLabel.text = bikeInRange ? "Bike in Range" : "Bike not in Range"
+        bikeImage.isHidden = !bikeInRange
+    }
+    
 }
 

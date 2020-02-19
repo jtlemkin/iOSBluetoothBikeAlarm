ea926aacb9eb43728a28c336bc318224c3384758
jtlemkin <jtlemkin@gmail.com>
2020-02-18 17:34:19 -0800

    Implemented basic bluetooth functionality

--- BikeAlarm/ViewController.swift
+++ BikeAlarm/ViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    var centralManager : CBCentralManager!
+    let alarmCBUUID = CBUUID(string: "temp")
     
     var armed = false
     var bikeInRange = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     @IBAction func ToggleAlarm(_ sender: Any) {
@@ -36,3 +41,30 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+          case .unknown:
+            print("central.state is .unknown")
+          case .resetting:
+            print("central.state is .resetting")
+          case .unsupported:
+            print("central.state is .unsupported")
+          case .unauthorized:
+            print("central.state is .unauthorized")
+          case .poweredOff:
+            print("central.state is .poweredOff")
+          case .poweredOn:
+            print("central.state is .poweredOn")
+            centralManager.scanForPeripherals(withServices: nil)
+        @unknown default:
+            print("unknown default")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+    }
+    
+    
+}
